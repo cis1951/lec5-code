d5eaaf29d2f573a5c0fd99b567eca0126e75fca3
Anthony Li <antli@seas.upenn.edu>
2025-10-01 16:55:08 -0400

    Update for 25fa

--- Minicourse Browser/Models/MockData.swift
+++ Minicourse Browser/Models/MockData.swift
@@ -8,22 +8,16 @@
 extension Course {
     static let minicourses: [Course] = [
         .init(
-            code: "CIS 1902",
-            name: "Python Programming",
-            description: "Python is an elegant, concise, and powerful language that is useful for tasks large and small. Python has quickly become a popular language for getting things done efficiently in many in all domains: scripting, systems programming, research tools, and web development. This course will provide an introduction to this modern high-level language using hands-on experience through programming assignments and a collaborative final application development project.",
-            icon: "applescript"
-        ),
-        .init(
             code: "CIS 1905",
             name: "Rust Programming",
             description: "Rust is a new, practical, community-developed systems programming language that \"runs blazingly fast, prevents almost all crashes, and eliminates data ra (rust-lang.org). Rust derives from a rich history of languages to create a multi-paradigm (imperative/functional), low-level language that focuses on high-performance, zero-cost safety guarantee in concurrent programs. It has begun to gain traction in industry, showing a recognized need for a new low-level systems language. In this course, we will cover what makes Rust so unique and apply it to practical systems programming problems. Topics covered will include traits and generics; memory safety (move semantics, borrowing, and lifetimes); Rust's rich macro system; closures; and concurrency. Evaluation is based on regular homework assignments as well as a final project and class participation. Prerequisite: CIS 1200 Recommended additional prerequisite: CIS 2400 or exposure to C or C++",
-            icon: "cpu"
+            icon: "gear"
         ),
         .init(
             code: "CIS 1912",
             name: "DevOps",
             description: "DevOps is the breaking down of the wall between Developers and Operations to allow more frequent and reliable feature deployments. Through a variety of automation-focused techniques, DevOps has the power to radically improve and streamline processes that in the past were manual and susceptible to human error. In this course we will take a practical, hands-on look at DevOps and dive into some of the main tools of DevOps: automated testing, containerization, reproducibility, continuous integration, and continuous deployment. Throughout the semester we build toward an end-to-end pipeline that takes a webserver, packages it, and then deploys it to the cloud in a reliable and quickly-reproducible manner utilizing industry-leading technologies like Kubernetes and Docker. Evaluation is based on homework assignments and a final group project.",
-            icon: "externaldrive.trianglebadge.exclamationmark"
+            icon: "xserve.raid"
         ),
         .init(
             code: "CIS 1921",
@@ -37,5 +31,17 @@ extension Course {
             description: "This project-oriented course is centered around application development on current iOS mobile platforms. The first half of the course will involve fundamentals of mobile app development, where students learn about mobile app lifecycles, event-based programming, efficient resource management, and how to interact with the range of sensors available on modern mobile devices. In the second half of the course, students work in teams to conceptualize and develop a significant mobile application. Creativity and originality are highly encouraged! Prerequisite: CIS 1200 or previous programming experience. ",
             icon: "iphone"
         ),
+        .init(
+            code: "CIS 1953",
+            name: "3D Game Development in Unreal Engine",
+            description: "The Unreal Engine is a state of the art tool for computer graphics creation. It provides an unparalleled number of rendering, physics, and design tools for developers. The Unreal Engine has become a staple in numerous fields from video games to films to health care to physical simulation to geospatial processing. This course will explore the application of Unreal Engine specifically in the development of 3D video games. Taking this class will foster proficiency in use of the engine, understanding of common practices and techniques in video game creation, and linear algebra and C++ in a game context. The course is recommended for computer science students interested in video game development, or prospective students to the Computer Graphics and Game Technology program.",
+            icon: "move.3d"
+        ),
+        .init(
+            code: "CIS 1962",
+            name: "JavaScript Programming",
+            description: "This course provides an introduction to modern web development frameworks, techniques, and practices used to deliver robust client side applications on the web. The emphasis will be on developing JavaScript programs that run in the browser. Topics covered include the JavaScript language, web browser internals, the Document Object Model (DOM), HTML5, client-side app architecture and compile-to-JS languages like (Coffeescript, TypeScript, etc.). This course is most useful for students who have some programming and web development experience and want to develop moderate JavaScript skills to be able to build complex, interactive applications in the browser.",
+            icon: "scroll"
+        )
     ]
 }
